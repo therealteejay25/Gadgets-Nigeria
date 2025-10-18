3c7c9c2a5b8914375c6174e1ceb147060a711a59
therealteejay25 <therealteejay25@gmail.com>
2025-10-18 12:43:21 +0100

    Removed clerk

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
@@ -13,7 +12,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
@@ -22,6 +20,5 @@ export default function RootLayout({ children }) {
           </AppContextProvider>
         </body>
       </html>
-      </ClerkProvider>
   );
 }
