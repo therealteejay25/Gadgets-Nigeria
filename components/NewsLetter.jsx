b6220538e2f59aafebfd3081fb257dce1a32a0b7
therealteejay25 <therealteejay25@gmail.com>
2025-10-18 13:06:22 +0100

    Email change

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -14,7 +14,7 @@ const NewsLetter = () => {
         <input
           className="border border-gray-500/30 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500"
           type="text"
-          placeholder="Enter your email id"
+          placeholder="Enter your email address..."
         />
         <button className="md:px-12 px-8 h-full text-white bg-[#15334B] rounded-md rounded-l-none">
           Subscribe
